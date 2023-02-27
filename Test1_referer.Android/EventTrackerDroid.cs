b7aa6e49d5844b7b92eee3e16de3684c4ad6999d
DanilPV <126478425+DanilPV@users.noreply.github.com>
2023-02-27 22:26:14 +0700

    Add files via upload

---
+++ Test1_referer.Android/EventTrackerDroid.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Android.OS;
+using Firebase.Analytics;
+using System.Collections.Generic;
+using Xamarin.Forms;
+using Test1_referer.Droid;
+
+[assembly: Dependency(typeof(EventTrackerDroid))]
+namespace Test1_referer.Droid
+{
+
+    public class EventTrackerDroid : IEventTracker
+    {
+        public void SendEvent(string eventId)
+        {
+            SendEvent(eventId, null);
+        }
+
+        public void SendEvent(string eventId, string paramName, string value)
+        {
+            SendEvent(eventId, new Dictionary<string, string>
+            {
+                {paramName, value}
+            });
+        }
+
+      
+        public void SendEvent(string eventId, IDictionary<string, string> parameters)
+        {
+            var firebaseAnalytics = FirebaseAnalytics.GetInstance(Forms.Context);
+
+            if (parameters == null)
+            {
+                firebaseAnalytics.LogEvent(eventId, null);
+                return;
+            }
+
+            var bundle = new Bundle();
+            foreach (var param in parameters)
+            {
+                bundle.PutString(param.Key, param.Value);
+            }
+
+            firebaseAnalytics.LogEvent(eventId, bundle);
+        }
+    }
+
+
+}
+
